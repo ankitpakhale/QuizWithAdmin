96b471145d88bf9dc72429d738f0ac0cdc8abc52
smart-fintech <staff.ddspl@gmail.com>
2022-06-02 14:10:59 +0530

    D_Change: admin.py

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,3 +2,13 @@ from django.contrib import admin
 from .models import Answer,question,Quiz,Testcategory,registerform,Record,Option,AdminForm
 # Register your models here.
 admin.site.register([Answer,question,Quiz,Testcategory,registerform,Record,Option,AdminForm])
+
+
+
+
+
+
+
+
+
+
